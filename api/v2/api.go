3373efc6046faa238b6fce9115715ff5d215f26f
Yijie Qin <qinyijie@amazon.com>
2023-06-19 19:59:19 -0400

    add api callback
    Signed-off-by: Yijie Qin <qinyijie@amazon.com>

--- api/v2/api.go
+++ api/v2/api.go
@@ -54,6 +54,7 @@ import (
 	"github.com/prometheus/alertmanager/silence"
 	"github.com/prometheus/alertmanager/silence/silencepb"
 	"github.com/prometheus/alertmanager/types"
+	"github.com/prometheus/alertmanager/util/callback"
 )
 
 var tracer = otel.Tracer("github.com/prometheus/alertmanager/api/v2")
@@ -66,6 +67,7 @@ type API struct {
 	alertGroups    groupsFn
 	getAlertStatus getAlertStatusFn
 	groupMutedFunc groupMutedFunc
+	apiCallback    callback.Callback
 	uptime         time.Time
 
 	// mtx protects alertmanagerConfig, setAlertStatus and route.
@@ -96,10 +98,14 @@ func NewAPI(
 	asf getAlertStatusFn,
 	gmf groupMutedFunc,
 	silences *silence.Silences,
+	apiCallback callback.Callback,
 	peer cluster.ClusterPeer,
 	l *slog.Logger,
 	r prometheus.Registerer,
 ) (*API, error) {
+	if apiCallback == nil {
+		apiCallback = callback.NoopAPICallback{}
+	}
 	api := API{
 		alerts:         alerts,
 		getAlertStatus: asf,
@@ -107,6 +113,7 @@ func NewAPI(
 		groupMutedFunc: gmf,
 		peer:           peer,
 		silences:       silences,
+		apiCallback:    apiCallback,
 		logger:         l,
 		m:              metrics.NewAlerts(r),
 		uptime:         time.Now(),
@@ -322,7 +329,13 @@ func (api *API) getAlertsHandler(params alert_ops.GetAlertsParams) middleware.Re
 		return *res[i].Fingerprint < *res[j].Fingerprint
 	})
 
-	return alert_ops.NewGetAlertsOK().WithPayload(res)
+	callbackRes, err := api.apiCallback.V2GetAlertsCallback(res)
+	if err != nil {
+		logger.Error("Failed to call api callback", "err", err)
+		return alert_ops.NewGetAlertsInternalServerError().WithPayload(err.Error())
+	}
+
+	return alert_ops.NewGetAlertsOK().WithPayload(callbackRes)
 }
 
 func (api *API) postAlertsHandler(params alert_ops.PostAlertsParams) middleware.Responder {
@@ -466,7 +479,13 @@ func (api *API) getAlertGroupsHandler(params alertgroup_ops.GetAlertGroupsParams
 		res = append(res, ag)
 	}
 
-	return alertgroup_ops.NewGetAlertGroupsOK().WithPayload(res)
+	callbackRes, err := api.apiCallback.V2GetAlertGroupsCallback(res)
+	if err != nil {
+		logger.Error("Failed to call api callback", "err", err)
+		return alertgroup_ops.NewGetAlertGroupsInternalServerError().WithPayload(err.Error())
+	}
+
+	return alertgroup_ops.NewGetAlertGroupsOK().WithPayload(callbackRes)
 }
 
 func (api *API) alertFilter(matchers []*labels.Matcher, silenced, inhibited, active bool) func(a *types.Alert, now time.Time) bool {
